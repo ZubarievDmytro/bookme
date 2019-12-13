4b3e5baa315a6a4856459ca666fb217777d521db
Dmytro Zubariev <spek1312@gmail.com>
2019-12-13 13:31:50 +0200

    Create user in DB on first login

--- client/src/GoogleAuth.js
+++ client/src/GoogleAuth.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Menu, Loader, Button } from 'semantic-ui-react';
 import { connect } from 'react-redux';
-import { SignIn, SignOut, fetchUsers } from './actions';
+import { SignIn, SignOut, fetchUsers, createUser } from './actions';
 
 class GoogleAuth extends React.Component {
     componentDidMount() {
@@ -10,7 +10,7 @@ class GoogleAuth extends React.Component {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId: '933713769986-sc19lpemkpj7p3kmq4l4d1knekrr1q7i.apps.googleusercontent.com',
-                scope: 'email'
+                scope: 'profile email'
             }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
                 this.onAuthChange(this.auth.isSignedIn.get());
@@ -21,8 +21,21 @@ class GoogleAuth extends React.Component {
 
     onAuthChange = isSignedIn => {
         if(isSignedIn) {
-            const user = this.props.users.filter(user => user.userId === this.auth.currentUser.get().getId());
-            this.props.SignIn(user[0].userId);
+            const userInfo = this.auth.currentUser.get();
+            const id = userInfo.getId();
+            let user = this.props.users.filter(user => user.userId === id);
+            
+            if (!user[0]) {
+                const name = userInfo.getBasicProfile().getName();
+                const userData = {
+                    id,
+                    name,
+                    "userId": id,
+                    "title": name
+                }
+                this.props.createUser(userData);
+            }
+            this.props.SignIn(id);
         } else {
             this.props.SignOut();
         }
@@ -58,4 +71,4 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps, { SignIn, SignOut, fetchUsers })(GoogleAuth);
+export default connect(mapStateToProps, { SignIn, SignOut, fetchUsers, createUser })(GoogleAuth);
--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,6 +2,17 @@ import { SIGN_IN, SIGN_OUT, FETCH_USERS, UPDATE_BOOKINGS, UPDATE_USER } from './
 import users from '../apis/users';
 import history from '../history';
 
+const initialUserData = {
+    "avatarUrl": "https://react.semantic-ui.com/images/wireframe/image.png",
+    "profession": "Job title",
+    "description": "There is no description yet",
+    "bookings": {
+      "shedule": [],
+      "myBookings": [],
+      "usersBookings": []
+    }
+}
+
 export const SignIn = userId => {
     return {
         type: SIGN_IN,
@@ -13,6 +24,12 @@ export const SignOut = () => {
     return {
         type: SIGN_OUT 
     }
+}
+
+export const createUser = userData => async dispatch => {
+    const res = await users.post(`/users?userId=${userData.userId}`, {...initialUserData, ...userData});
+
+    dispatch({type: UPDATE_USER, payload: res.data})
 }
 
 export const fetchUsers = () => async dispatch => {
